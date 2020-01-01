7b619641f0a048c557c332340e7902e7661bdb36
chenlulu <707010543@qq.com>
2020-01-01 16:56:40 +0800

    update fileinfo struct

--- examples/json_test/main.go
+++ examples/json_test/main.go
@@ -11,21 +11,24 @@ import (
 
 
 func main()  {
-	p := &dbmanager.FileInfo{}
-    p.Name = "pgutil"
-    p.IsDir = true
-    p.FileSize = 10000
-    p.LatestModify = "2499.00"
-    p.FileUrl = "1"
-    data, _ := json.Marshal(p)
-	fmt.Println(string(data))
+	p := &dbmanager.PgFileInfo{}
+    // p.Name = "pgutil"
+    // p.IsDir = true
+    // p.FileSize = 10000
+    // p.LatestModify = 10000
+    // p.FileUrl = "1"
+    // data, _ := json.Marshal(p)
+	// fmt.Println(string(data))
 	if len(os.Args) > 1{
 		flag.Parse()
 		root := flag.Arg(0)
 		arrdir := fileutil.GetFilelist(root, "zip", false)
 		for i := 0; i < len(arrdir); i++ {
 			fmt.Print(arrdir[i], "\t\t")
-			fmt.Println(fileutil.GetFileSize(arrdir[i]), fileutil.GetFileModTime(arrdir[i]))
+			p.MarshalJSONInfo(fileutil.GetFullName(arrdir[i]), arrdir[i], fileutil.GetFileModTime(arrdir[i]), fileutil.IsDirectory(arrdir[i]), fileutil.GetFileSize(arrdir[i]))
+			data, _ := json.Marshal(p)
+			fmt.Println(string(data))
+			//fmt.Println(fileutil.GetFileSize(arrdir[i]), fileutil.GetFileModTime(arrdir[i]))
 		}
 	}else{
 		fmt.Println("please input a root dir!")
