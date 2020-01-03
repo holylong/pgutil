296fff579b4eac3c85e286133eeb351d07d23e22
lcmftianci <707010543@qq.com>
2020-01-03 18:12:11 +0800

    update tester

--- examples/file_test/main.go
+++ examples/file_test/main.go
@@ -11,8 +11,8 @@ func main(){
 	if len(os.Args) > 1{
 		flag.Parse()
 		root := flag.Arg(0)
-		//arrdir := fileutil.GetFilelist(root, "zip", false)
-		arrdir := fileutil.ReverseFileList(root)
+		arrdir := fileutil.GetFilelist(root, ".git", true)
+		//arrdir := fileutil.ReverseFileList(root)
 		for i := 0; i < len(arrdir); i++ {
 			fmt.Print(arrdir[i], "\n")
 			os.RemoveAll(arrdir[i])
